2a1c40b411a9d1e774959df5882051ea78330bef
gzgzapp <137661695@qq.com>
2017-11-08 16:34:44 +0800

    提交和取消提供回调函数，schema设置隐藏和input不可用，提供clearLoading方法用于提交成功或者失败后的清楚loading状态

--- self/popupForm/popupForm.js
+++ self/popupForm/popupForm.js
@@ -1,10 +1,10 @@
-(function($){
-    var popupForm = function(eleType, options){
+(function ($) {
+    var popupForm = function (eleType, options) {
         this.options = options;
-        this.show = function(data){
-            if(data){//编辑状态，把值传进去
+        this.show = function (data) {
+            if (data) {//编辑状态，把值传进去
                 this.title = "Edit " + eleType;
-            }else{
+            } else {
                 this.title = "Add " + eleType;
             }
             this.$popup || this.createPopupForm(options);
@@ -13,27 +13,58 @@
             this.$popup.show();
             this.resize();
         };
-        this.putData = function(data){//{TIKD_ID:3332234444,TIKV_NAME:DSDSDSDSDS ...}
-            if(data == null ){
+        this.formToJson = function () {
+            var data = "{", that = this, len = this.options.data.length, isAppend = true;
+            $.each(this.options.data, function (index, row) {
+                for (var id in row) {
+                    var type = row[id]["type"];
+                    if (index < len - 1 && index != 0 && isAppend) {
+                        data += ",";
+                    }
+                    isAppend = true;
+                    if (type === "string") {
+                        data += "\"" + id + "\":" + "\"" + that.$form.find("input[name='" + id + "']").val() + "\"";
+                    } else if (type === "array" || type === "object") {
+                        isAppend = false;
+                        continue;
+                    } else {
+                        data += "\"" + id + "\":" + that.$form.find("input[name='" + id + "']").val();
+                    }
+                }
+            });
+            data += "}";
+            return data;
+        };
+        this.putData = function (data) {//{TIKD_ID:3332234444,TIKV_NAME:DSDSDSDSDS ...}
+            if (data == null) {
                 return;
             }
-            for(var key in data){
-                this.$form.find("input[name='"+key+"']").val(data[key]);
+            for (var key in data) {
+                this.$form.find("input[name='" + key + "']").val(data[key]);
             }
         };
-        this.hide = function(){
+        this.hide = function () {
             this.$popup && this.$popup.hide();
         };
-        this.resize = function(){//会随浏览器大小自动居中和调整popup的大小
-            this.$popupBody.css({
-                "margin-left":-(this.$popupBody.width()/2)+"px",
-                "margin-top":-(this.$popupBody.height()/2)+"px"});//居中效果
-            var padding = this.$popupContent.css("padding-top").split("px")[0];
-            this.$popupBody.height(this.$popupTitle.height() + this.contentHeight + this.$popupFoot.height()+3);//3是border的高度
-            this.$popupBody.width(this.contentWidth);
-            this.$popupContent.height(this.$popupBody.height() - this.$popupTitle.height() - this.$popupFoot.height() - padding *2);//30是2 * padding的高度
+        this.clearLoading = function () {
+            this.$popupBody.css("z-index", 1000);
+            this.$loading.hide();
         };
-        this.createPopupForm = function(options){
+        this.startLoading = function () {
+            this.$popupBody.css("z-index", 998);
+            this.$loading.show();
+        };
+        this.resize = function () {//会随浏览器大小自动居中和调整popup的大小
+            this.$popupBody.css({
+                "margin-left": -(this.$popupBody.width() / 2) + "px",
+                "margin-top": -(this.$popupBody.height() / 2) + "px"
+            });//居中效果
+            var padding = this.$popupContent.css("padding-top").split("px")[0];
+            this.$popupBody.height(this.$popupTitle.height() + this.contentHeight + this.$popupFoot.height() + 3);//3是border的高度
+            this.$popupBody.width(this.contentWidth);
+            this.$popupContent.height(this.$popupBody.height() - this.$popupTitle.height() - this.$popupFoot.height() - padding * 2);//30是2 * padding的高度
+        };
+        this.createPopupForm = function (options) {
             var that = this;
             this.createForm(options),
                 this.$popup = $("<div class='popup'></div>"),
@@ -43,74 +74,82 @@
                 this.$popupClose = $("<span class='popupBodyClose'>x</span>"),
                 this.$popupContent = $("<div class = 'popupBodyContent'></div>"),
                 this.$popupFoot = $("<div class = 'popupBodyFoot'></div>"),
-                this.$cancelBtn = $("<button type = 'button' class = 'popupBodyFootBtn' >取消</button>");
-                this.$submitBtn = $("<button type = 'button' class = 'popupBodyFootBtn' >提交修改</button>");
+                this.$cancelBtn = $("<button type = 'button' class = 'popupBodyFootBtn' >取消</button>"),
+                this.$submitBtn = $("<button type = 'button' class = 'popupBodyFootBtn' >提交修改</button>"),
+                this.$loading = $("<div class='popupLoading' style='display: none'></div>");
 
             this.$popupContent.append(this.$form);
-            this.$popupFoot.append(this.$submitBtn,this.$cancelBtn);
-            this.$popupBody.append(this.$popupTitle,this.$popupClose,this.$popupContent,this.$popupFoot);
-            this.$popup.append(this.$popupShelter,this.$popupBody);
+            this.$popupFoot.append(this.$submitBtn, this.$cancelBtn);
+            this.$popupBody.append(this.$popupTitle, this.$popupClose, this.$popupContent, this.$popupFoot);
+            this.$popup.append(this.$popupShelter, this.$popupBody);
             this.$popup.appendTo($('body'));
+            this.$loading.appendTo($('body'));
 
             this.contentHeight = this.$popupContent.height();
             this.contentWidth = this.$popupContent.width();
             //绑定事件
-            $(window).resize(function() {
+            $(window).resize(function () {
                 that.resize();
             });
-            this.$cancelBtn.click(function(){
+            this.$cancelBtn.click(function () {
                 that.hide();
+                that.options.cancelCallBack();
             });
-            this.$popupClose.click(function(){
+            this.$popupClose.click(function () {
                 that.hide();
+                that.options.cancelCallBack();
             });
-            this.$submitBtn.click(function(){
-                that.$popupBody.css("z-index",998);
-                var disableds = that.$form.find("input[disabled=true]").removeAttr("disabled");
-                var data = that.$form.serialize();
-                disableds.attr("disabled",true);
-                $.ajax({
-                    type:"post",
-                    url:that.options.url,
-                    data:that.$form.serialize(),
-                    success:function(data){
-                        that.$popupBody.css("z-index",1000);
-                        disableds.removeAttr("disabled");
-                        that.$form[0].reset();
-                        disableds.attr("disabled",true);
-                        that.options.successCallback(data);
-                        that.hide();
-                    },
-                    error:function(msg){
-                        that.$popupBody.css("z-index",1000);
-                        that.options.errorCallBack(msg);
-                    }
-                });
+            this.$submitBtn.click(function () {
+                var data = that.formToJson();
+                that.startLoading();
+                that.options.submitCallBack(data);
+                /*that.$popupBody.css("z-index",998);
+                 var disableds = that.$form.find("input[disabled=true]").removeAttr("disabled");
+                 var data = that.$form.serialize();
+                 disableds.attr("disabled",true);
+                 $.ajax({
+                 type:"post",
+                 url:that.options.url,
+                 data:that.$form.serialize(),
+                 success:function(data){
+                 that.$popupBody.css("z-index",1000);
+                 disableds.removeAttr("disabled");
+                 that.$form[0].reset();
+                 disableds.attr("disabled",true);
+                 that.options.successCallback(data);
+                 that.hide();
+                 },
+                 error:function(msg){
+                 that.$popupBody.css("z-index",1000);
+                 that.options.errorCallBack(msg);
+                 }
+                 });*/
             });
             this.resize();
             this.hide();
         };
-        this.createForm = function(options){
-            var that = this,data = options['data'];
-            this.$form = $("<form class='popupFrom' action='"+options.url+"' method='post'></form>");
-            for(var i in data){
+        this.createForm = function (options) {
+            var that = this, data = options['data'];
+            this.$form = $("<form class='popupFrom'></form>");
+            for (var i in data) {
                 var row = data[i];
-                for(var id in row){
+                for (var id in row) {
                     var field = row[id],
                         label = field['description'],
                         required = field['required'],
                         minLength = field['minLength'],
                         type = field['type'],
                         pattern = field['pattern'],
-                        filedset = $("<fieldset class='popupFormFiedSet'>" +
-                            "<div class='popupFromGroup'>" +
-                                "<label class='popupFormLabel'>"+label+"</label>"+
-                                "<div class = 'popupFormDivInput'>" +
-                            //(id === "TIKV_ID" ? "<input class = 'popupFormInput' name='"+id+"' disabled=true value='135h5jdf-3d3e3-dddf3-665fdsd'>" :
-                                    "<input class = 'popupFormInput' name='"+id+"' />" +
-                                "</div>"+
-                            "</div>" +
-                            "</fieldset>").appendTo(that.$form);
+                        disabled = field['inputDisabled'];
+                    label && $("<fieldset class='popupFormFiedSet'>" +
+                        "<div class='popupFromGroup'>" +
+                        "<label class='popupFormLabel'>" + label + "</label>" +
+                        "<div class = 'popupFormDivInput'>" +
+                        (disabled === true ? "<input class = 'popupFormInput' name='" + id + "' disabled=true >" :
+                        "<input class = 'popupFormInput' name='" + id + "' />") +
+                        "</div>" +
+                        "</div>" +
+                        "</fieldset>").appendTo(that.$form);
 
                 }
             }
@@ -119,39 +158,39 @@
         };
     };
     $.extend({
-        popupForm :function(eleType ,url ,schema,successCallback,errorCallBack){
+        popupForm: function (eleType, schema, submitCallBack, cancelCallBack) {
             var eleTypeJson,
-                fields=[],
-                options={
-                    url:url,
-                    successCallback:successCallback,
-                    errorCallBack:errorCallBack
+                fields = [],
+                options = {
+                    submitCallBack: submitCallBack,
+                    cancelCallBack: cancelCallBack
                 },
-                res={};
-            analysis(eleType,schema['properties']);
+                res = {};
+            analysis(eleType, schema['properties']);
             getFields(eleTypeJson);
             console.log(fields);
             options.data = fields;
-            function analysis(eleType ,schema){
-                if(eleTypeJson){
+            function analysis(eleType, schema) {
+                if (eleTypeJson) {
                     return;
                 }
-                if(schema[eleType]){
+                if (schema[eleType]) {
                     eleTypeJson = schema[eleType];
                     return;
                 }
-                for(var key in schema){
+                for (var key in schema) {
                     var jsonRoot = schema[key]['properties'];
-                    jsonRoot && analysis(eleType,jsonRoot);
+                    jsonRoot && analysis(eleType, jsonRoot);
                 }
             }
-            function getFields(json){
+
+            function getFields(json) {
                 var type = json['type'];
-                if(type === 'array'){
+                if (type === 'array') {
                     getFields(json['items']);
-                }else if(type === 'object'){
+                } else if (type === 'object') {
                     var properties = json['properties'];
-                    for(var key in properties){
+                    for (var key in properties) {
                         var field = new Object();
                         field[key] = properties[key];
                         /*var option = {key:properties[key]};*/
@@ -159,7 +198,8 @@
                     }
                 }
             }
-            res = new popupForm(eleType,options);
+
+            res = new popupForm(eleType, options);
             return res;
         }
     });
